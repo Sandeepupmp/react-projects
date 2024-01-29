88f07cbe64d56f5f8485e2dc9833ca47822b3ca3
sandeep <sandeepupmp@gmail.com>
2024-01-29 20:13:51 +0530

    g

--- side-bar/src/Sidebar.jsx
+++ side-bar/src/Sidebar.jsx
@@ -1,8 +1,44 @@
 import {social, links} from "./Data"
+import { FaTimes } from "react-icons/fa";
 import logo from './logo.svg'
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <aside className={`sidebar show-sidebar`}>
+        <div className="sidebar-header">
+            <img src={logo} alt="logo" className="logo" />
+            <button className="close-btn">
+                <FaTimes/>
+            </button>
+
+        </div>
+        <ul className="links">
+            {links.map((item)=>{
+                const {id, text, url ,icon}= item
+                return(
+                    <li key={id}>
+                        <a href={url}>
+                            {icon}
+                            {text}
+                        </a>
+                    </li>
+                )
+            })}
+
+        </ul>
+        <ul className="social-icons">
+          {social.map((link)=>{
+            const {id, url , icon} = link
+            return(
+                <li key={id}>
+                    <a href={url}>
+                        {icon}
+                    </a>
+                </li>
+            )
+          })}
+        </ul>
+
+    </aside>
   )
 }
 export default Sidebar
