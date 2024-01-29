6fb99ffe124008b39fe2e6af0b56ce249d93b60b
sandeep <sandeepupmp@gmail.com>
2024-01-29 22:11:04 +0530

    fuck yar error aa raha hai

---
+++ context Api/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './component/Login'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Context API</h1>
+      <Login/>
+    </>
+  )
+}
+
+export default App
