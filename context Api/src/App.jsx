23b1777ab5b2560a15dec945fb622b6d4f4626ea
sandeep <sandeepupmp@gmail.com>
2024-01-29 22:37:00 +0530

    now project has been working and scend project started

--- context Api/src/App.jsx
+++ context Api/src/App.jsx
@@ -3,15 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './component/Login'
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './component/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <h1>Context API</h1>
       <Login/>
-    </>
+      <Profile/>
+    </UserContextProvider>
   )
 }
 
--- context Api/src/component/Profile.jsx
+++ context Api/src/component/Profile.jsx
@@ -1,6 +1,12 @@
+import React, {useContext} from 'react'
+import UserContext from '../context/UserContext'
+
 function Profile() {
-  return (
-    <div>Profile</div>
-  )
+    const {user} = useContext(UserContext)
+    
+    if (!user) return <div>please login</div>
+
+    return <div>Welcome {user.username}</div>
 }
+
 export default Profile
