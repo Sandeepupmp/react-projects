0f72e92ebc5b0be6447607fcf80ef7ab66e70a22
sandeep <sandeepupmp@gmail.com>
2024-01-30 22:00:18 +0530

    i going to make linkdin clone using react and firebas let see how many days take to comoplite this project

---
+++ Linkdin-clone/src/FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApK5ooDGN8Y79RxJBFzk72QV9xvpDYaGU",
+  authDomain: "linkdin-clone-a0284.firebaseapp.com",
+  projectId: "linkdin-clone-a0284",
+  storageBucket: "linkdin-clone-a0284.appspot.com",
+  messagingSenderId: "886703436132",
+  appId: "1:886703436132:web:18cf2dfdaf9b183a2a4a71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- context-project/src/App.jsx
+++ context-project/src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+
 
 function App() {
   
 
   return (
-    <>
-      <h1>Context</h1>
-    </>
+   
+            <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
+
   )
 }
 
